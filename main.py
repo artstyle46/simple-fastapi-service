f8f0ece805f3dadfa6a5d63c91bd32d4d4519e50
artstyle46 <asit1992@gmail.com>
2022-03-21 12:36:58 +0530

    push post api

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from typing import Optional, Any
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Data(BaseModel):
+    data: Optional[Any] = None
+
+
+@app.post("/")
+async def root(data: Data):
+    return data
